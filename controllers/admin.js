b9953a4fafb0614cc75628e1618153b55ef25848
Alan Felipe Ferreira <alan.fferreira03@gmail.com>
2020-02-07 19:24:43 -0300

    Commit inicial

---
+++ controllers/admin.js
@@ -0,0 +1,64 @@
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+    res.render('admin/edit-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        editing: false
+    });
+};
+
+exports.postAddProduct = (req, res, next) => {
+    const title = req.body.title;
+    const imgUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description
+    const product = new Product(null, title, imgUrl, price, description);
+    product.save();
+    res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    const prodId = req.params.productId;
+
+    if(!editMode){
+        return res.redirect('/')
+    }
+
+    Product.findById(prodId, product => {
+        res.render('admin/edit-product', {
+            pageTitle: "Edit Product",
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+        });    
+    });
+}
+
+exports.postEditProduct = (req, res, next) => {
+    const id = req.body.id;
+    const title = req.body.title;
+    const imgUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description
+    const product = new Product(id, title, imgUrl, price, description);
+    product.save();
+    res.redirect('/admin/products');
+}
+
+exports.postDeleteProductById = (req, res, next) => {
+    const prodId = req.body.prodId;
+    Product.deleteById(prodId);
+    res.redirect('/admin/products')
+}
+
+exports.getAdminProducts = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('admin/products',{
+            prods: products,
+            pageTitle: 'Admin Products',
+            path: '/admin/products'
+        });
+    });
+};
